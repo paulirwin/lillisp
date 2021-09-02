1886ed044ef2898581f6fa5850bded5bc251a34c
Paul Irwin <paulirwin@gmail.com>
2021-09-02 17:12:34 -0600

    Add more unit tests

---
+++ Lillisp.Tests/DynamicTests.cs
@@ -0,0 +1,35 @@
+﻿using Lillisp.Core;
+using Xunit;
+
+namespace Lillisp.Tests
+{
+    public class DynamicTests
+    {
+        [InlineData("(count '())", 0)]
+        [InlineData("(count nil)", 0)]
+        [InlineData("(count '(1 2 3))", 3)]
+        [InlineData("(count \"foo bar\")", 7)]
+        [Theory]
+        public void CountTests(string input, int expected)
+        {
+            var runtime = new LillispRuntime();
+
+            var result = runtime.EvaluateProgram(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [InlineData("(get '(1 2 3) 0)", 1d)]
+        [InlineData("(get (range 0 10 2) 2)", 4d)]
+        [InlineData("(get \"foo bar\" 5)", 'a')]
+        [Theory]
+        public void GetTests(string input, object expected)
+        {
+            var runtime = new LillispRuntime();
+
+            var result = runtime.EvaluateProgram(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Lillisp.Tests/InteropTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using Lillisp.Core;
+using Xunit;
+
+namespace Lillisp.Tests
+{
+    public class InteropTests
+    {
+        [InlineData("(begin (use 'System.Text) (typeof (new StringBuilder)))", typeof(StringBuilder))]
+        [InlineData("(typeof (new Uri \"https://www.google.com\"))", typeof(Uri))]
+        [InlineData("(begin (def x (new DateTime)) (typeof x))", typeof(DateTime))]
+        [Theory]
+        public void NewAndUseTests(string input, object expected)
+        {
+            var runtime = new LillispRuntime();
+
+            var result = runtime.EvaluateProgram(input);
+
+            Assert.Equal(expected, result);
+        }
+
+        [InlineData("Math/PI", Math.PI)]
+        [InlineData("(begin (def x String/Empty) x)", "")]
+        [InlineData("(String/IsNullOrEmpty null)", true)]
+        [InlineData("(String/IsNullOrEmpty \"foo\")", false)]
+        [Theory]
+        public void StaticMemberTests(string input, object expected)
+        {
+            var runtime = new LillispRuntime();
+
+            var result = runtime.EvaluateProgram(input);
+
+            Assert.Equal(expected, result);
+        }
+        
+        [InlineData("(begin (def x (new Uri \"https://www.google.com\")) (.Scheme x))", "https")]
+        [InlineData("(begin (use 'System.Text) (def x (new StringBuilder)) (.Append x \"foo\") (.Append x \"bar\") (str x))", "foobar")]
+        [Theory]
+        public void InstanceMemberTests(string input, object expected)
+        {
+            var runtime = new LillispRuntime();
+
+            var result = runtime.EvaluateProgram(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
