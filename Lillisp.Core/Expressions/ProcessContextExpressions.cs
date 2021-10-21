7c2db4536fb9026c83c35e4f69b148463ebaf424
Paul Irwin <paulirwin@gmail.com>
2021-10-21 17:26:06 -0600

    get-environment-variable and emergency-exit expressions

---
+++ Lillisp.Core/Expressions/ProcessContextExpressions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace Lillisp.Core.Expressions
+{
+    public static class ProcessContextExpressions
+    {
+        public static object? GetEnvironmentVariable(object?[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException("get-environment-variable requires one argument");
+            }
+
+            return args[0] switch
+            {
+                string s => Environment.GetEnvironmentVariable(s),
+                StringBuilder sb => Environment.GetEnvironmentVariable(sb.ToString()),
+                _ => throw new ArgumentException("name must be a string")
+            };
+        }
+
+        public static object? EmergencyExit(object?[] args)
+        {
+            if (args.Length == 0)
+            {
+                Environment.Exit(0);
+                return null;
+            }
+
+            if (args[0] is bool b)
+            {
+                if (b)
+                {
+                    Environment.Exit(0);
+                    return null;
+                }
+
+                Environment.Exit(-1);
+                return null;
+            }
+
+            try
+            {
+                int code = Convert.ToInt32(args[0]);
+                Environment.Exit(code);
+            }
+            catch
+            {
+                Environment.Exit(-1);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Lillisp.Tests/ProcessContextTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Lillisp.Core;
+using Xunit;
+
+namespace Lillisp.Tests
+{
+    public class ProcessContextTests
+    {
+        [Fact]
+        public void GetEnvironmentVariable_BasicTest()
+        {
+            var runtime = new LillispRuntime();
+
+            Environment.SetEnvironmentVariable("foo", "bar");
+
+            var result = runtime.EvaluateProgram("(get-environment-variable \"foo\")");
+
+            Assert.Equal("bar", result);
+        }
+    }
+}
