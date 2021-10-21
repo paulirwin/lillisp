d9dbc50be4530faf7f655ea01258e5f7a197ee33
Paul Irwin <paulirwin@gmail.com>
2021-10-21 17:12:21 -0600

    Jiffies!

---
+++ Lillisp.Core/Expressions/TimeExpressions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lillisp.Core.Expressions
+{
+    public static class TimeExpressions
+    {
+        public static object? CurrentJiffy(object?[] args)
+        {
+            return DateTime.UtcNow.Ticks;
+        }
+
+        public static object? JiffiesPerSecond(object?[] args)
+        {
+            return TimeSpan.TicksPerSecond;
+        }
+    }
+}
---
+++ Lillisp.Tests/TimeTests.cs
@@ -0,0 +1,30 @@
+﻿using Lillisp.Core;
+using Xunit;
+
+namespace Lillisp.Tests
+{
+    public class TimeTests
+    {
+        [Fact]
+        public void CurrentJiffy_BasicTest()
+        {
+            var runtime = new LillispRuntime();
+
+            var result = runtime.EvaluateProgram("(current-jiffy)");
+
+            Assert.IsType<long>(result);
+            Assert.NotEqual(0L, result);
+        }
+
+        [Fact]
+        public void JiffiesPerSecond_BasicTest()
+        {
+            var runtime = new LillispRuntime();
+
+            var result = runtime.EvaluateProgram("(jiffies-per-second)");
+
+            Assert.IsType<long>(result);
+            Assert.Equal(10_000_000L, result);
+        }
+    }
+}
